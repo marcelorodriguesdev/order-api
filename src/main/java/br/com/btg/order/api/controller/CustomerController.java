0128d2e84bd6a44cea75e9d7506c804430d84e5f
Marcelo Rodrigues <violontchelo@MacBook-Air-de-Marcelo.local>
2023-10-02 19:01:29 -0300

    ajustes finais

--- src/main/java/br/com/btg/order/api/controller/CustomerController.java
+++ src/main/java/br/com/btg/order/api/controller/CustomerController.java
@@ -2,12 +2,9 @@ package br.com.btg.order.api.controller;
 
 import br.com.btg.order.api.response.CustomerOrderQuantityResponse;
 import br.com.btg.order.core.service.CustomerService;
-import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import static br.com.btg.order.api.response.ErrorResponse.ERROR_RESPONSE_EXCEPTION_MODEL;
 
 @RestController
 @RequestMapping("/v1/customers")
@@ -18,12 +15,6 @@ public class CustomerController {
 
     @GetMapping(value = "/{customerId}/orders_quantity")
     @ResponseStatus(HttpStatus.OK)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Quantity of orders returned successfully."),
-            @ApiResponse(code = 404, message = "Customer not found.", examples = @Example(value = @ExampleProperty(mediaType = "application/json", value = ERROR_RESPONSE_EXCEPTION_MODEL))),
-            @ApiResponse(code = 500, message = "Internal server error", examples = @Example(value = @ExampleProperty(mediaType = "application/json", value = ERROR_RESPONSE_EXCEPTION_MODEL))),
-    }
-    )
     public CustomerOrderQuantityResponse getOrderQuantityForCustomer(@PathVariable("customerId") Long customerId) {
         return customerService.getOrderQuantityForCustomerById(customerId);
     }
